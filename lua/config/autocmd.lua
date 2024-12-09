8f1e125aacdb01a787cdd05b68494b9fb01501e6
Yeeloman <yami.onlyme@gmail.com>
2024-12-09 16:15:39 +0100

    adding auto indent on save

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_exec(
-	[[
+  [[
   augroup MyCursorShapes
     autocmd!
     autocmd InsertEnter * set guicursor=n-v-c:ver25,i-ci-ve:ver25
@@ -7,13 +7,21 @@ vim.api.nvim_exec(
     autocmd VimLeave * set guicursor=n-v-c:block,i-ci-ve:block
   augroup END
 ]],
-	false
+  false
 )
 -- Restore cursor position
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.api.nvim_exec('silent! normal! g`"zv', false)
-	end,
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_exec('silent! normal! g`"zv', false)
+  end,
 })
 
+
+-- Auto indent
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("normal gg=G")
+  end,
+})
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,10 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-      }
+      },
+      indent = { 
+        enable = true,
+      },
     })
   end,
 }
