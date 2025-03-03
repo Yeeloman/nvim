f199bedf54c70be6ad1a86df593da40dc645e7f0
Yeeloman <yami.onlyme@gmail.com>
2025-03-03 02:01:58 +0000

    adding find symbols keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,6 +48,7 @@ return {
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fa', builtin.lsp_document_symbols, { desc = 'Telescope find functions' })
       vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope Git files' })
       -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end,
