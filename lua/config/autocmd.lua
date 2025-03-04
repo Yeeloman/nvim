30cdfa884c17309ebbdb874d809646077420d383
Yeeloman <yami.onlyme@gmail.com>
2025-03-04 15:01:24 +0000

    nothing changed

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -39,6 +39,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	end
 })
 
+
 -- Open mini-files if nvim is opened with a dir as an argument
 -- vim.api.nvim_create_autocmd("VimEnter", {
 -- 	callback = function()
